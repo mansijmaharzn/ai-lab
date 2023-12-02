9dd493995b9c619f15a2023661d43a8181058bbb
Mansij Maharjan <mansijmaharzn@gmail.com>
2023-12-02 21:04:43 +0545

    fix minor bugs

--- lab-1/vaccum_cleaner.py
+++ lab-1/vaccum_cleaner.py
@@ -1,6 +1,6 @@
 def clean_room(location, status, goal_state, cost):
     print(f"Cleaner is in location {location}.")
-    
+
     if status == 1:
         print("Room is dirty. Started Cleaning...")
         goal_state[location] = '0'
@@ -8,15 +8,14 @@ def clean_room(location, status, goal_state, cost):
         print("Room cleaned.")
     else:
         print(f"Room {location} is already cleaned.")
-    
-    return cost, goal_state
 
+    return cost, goal_state
 
 def main():
     goal_state = {'A': '0', 'B': '0'}
     cost = 0
 
-    initial_location = input("Enter location of vaccum: (A or B): ").upper()[0]
+    initial_location = input("Enter location of vacuum: (A or B): ").upper()[0]
     complement_location = 'B' if initial_location == 'A' else 'A'
 
     initial_location_status = int(input(f"Enter '0' for clean or '1' for dirty, for room {initial_location}: "))
@@ -25,14 +24,16 @@ def main():
     cost, updated_goal_state = clean_room(initial_location, initial_location_status, goal_state, cost)
     goal_state = updated_goal_state
 
-    print(f"Moved to location {complement_location}.")
-    cost += 1
+    if initial_location_complement_status == 1:
+        print(f"Moved to location {complement_location}.")
+        cost += 1
 
-    cost, updated_goal_state = clean_room(complement_location, initial_location_complement_status, goal_state, cost)
-    goal_state = updated_goal_state
+        cost, updated_goal_state = clean_room(complement_location, initial_location_complement_status, goal_state, cost)
+        goal_state = updated_goal_state
+    else:
+        print(f"{complement_location} is already clean.")
 
     print(f"Total cost: Rs. {cost}.")
     print(f"Final goal state: {goal_state}")
-
 
 main()
