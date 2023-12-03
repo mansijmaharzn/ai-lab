730a7273495f8c762134babd5a40e23ed399f5f7
Mansij Maharjan <mansijmaharzn@gmail.com>
2023-12-03 19:07:07 +0545

    remove unreq variable

--- lab-1/vaccum_cleaner.py
+++ lab-1/vaccum_cleaner.py
@@ -11,6 +11,7 @@ def clean_room(location, status, goal_state, cost):
 
     return cost, goal_state
 
+
 def main():
     goal_state = {'A': '0', 'B': '0'}
     cost = 0
@@ -21,15 +22,13 @@ def main():
     initial_location_status = int(input(f"Enter '0' for clean or '1' for dirty, for room {initial_location}: "))
     initial_location_complement_status = int(input(f"Enter '0' for clean or '1' for dirty, for room {complement_location}: "))
 
-    cost, updated_goal_state = clean_room(initial_location, initial_location_status, goal_state, cost)
-    goal_state = updated_goal_state
+    cost, goal_state = clean_room(initial_location, initial_location_status, goal_state, cost)
 
     if initial_location_complement_status == 1:
         print(f"Moved to location {complement_location}.")
         cost += 1
 
-        cost, updated_goal_state = clean_room(complement_location, initial_location_complement_status, goal_state, cost)
-        goal_state = updated_goal_state
+        cost, goal_state = clean_room(complement_location, initial_location_complement_status, goal_state, cost)
     else:
         print(f"{complement_location} is already clean.")
 
